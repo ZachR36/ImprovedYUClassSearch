cfadc011608fca8c5dec9c1377e6d087831c22cb
Eytan Sheinfeld <eytan.sheinfeld@gmail.com>
2026-04-28 17:32:04 -0400

    Started Search class

---
+++ Search.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * Search
+ */
+public class Search {
+  private static HashMap<Integer, Course> mapByCRN = new HashMap<>();
+
+  public static void main(String[] args) {
+    try {
+      setup(args[0]);
+    } catch (Exception IOException) {
+      System.out.println("Failed to read CSV");
+    }
+  }
+
+  private static void setup(String path) throws IOException {
+    Path csv = Path.of(path);
+    try (BufferedReader reader = Files.newBufferedReader(csv)) {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        Course newCourse = new Course(line);
+        mapByCRN.put(newCourse.getCRN(), newCourse);
+      }
+    }
+  }
+}
